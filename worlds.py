82d3d9ae82168f4866af0c3683c0eeb47e26eb78
KenyC <keny.chatain@gmail.com>
2018-11-11 08:41:28 -0500

    Tutorial

--- worlds.py
+++ worlds.py
@@ -1,4 +1,5 @@
 from assignment import getAssignment
+from prettytable.prettytable import PrettyTable, PLAIN_COLUMNS
 import numpy as np
 
 class Universe:
@@ -22,6 +23,24 @@ class Universe:
 
 	def evaluate(self, *fs):
 		return np.transpose(np.stack([f.evaluate(self.worlds) for f in fs]))
+
+	def truthTable(self, *fs):
+		output = self.evaluate(*fs)
+
+		t = PrettyTable()
+		t.field_names = [" A"+str(i) +" " for i in range(self.n)] + [str(f) for f in fs]
+		# t.set_style(PLAIN_COLUMNS)
+
+		for i in range(len(output)):
+			t.add_row(list(self.worlds[i].astype("int"))+list(output[i].astype("int")))
+
+		print(t)
+
+	def equivalent(self,f,g):
+		return self.evaluate(f) == self.evaluate(g)
+
+	def consistent(self, *l):
+		return np.amax(np.amin(self.evaluate(*l), axis = 1), axis = 0)
 
 	def restrict(self, indices):
 		return Universe(self.n, self.worlds[indices])
