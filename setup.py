4dda9b82f92cdaba8f540a40a96eb940cdea811d
KenyC <keny.chatain@gmail.com>
2020-12-10 19:33:46 +0100

    respaced setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
 import exh
 from setuptools import setup
 
-setup(name             = "Exh",
-      version          = exh.__version__,
-      description      = "Computes innocent exclusion/inclusions exhaustivity",
-      url              = "http://github.com/KenyC/Exh",
-      author           = "Keny Chatain",
-      author_email     = "kchatain@mit.edu",
-      license          = "MIT",
-      packages         = ["exh", 
-                          "exh.model", 
-                          "exh.utils", 
-                          "exh.prop", 
-                          "exh.fol"],
-      install_requires = [
-          "numpy",
-          "IPython"
-      ],
-      zip_safe         = True)
+setup(
+    name             = "Exh",
+    version          = exh.__version__,
+    description      = "Computes innocent exclusion/inclusions exhaustivity",
+    url              = "http://github.com/KenyC/Exh",
+    author           = "Keny Chatain",
+    author_email     = "kchatain@mit.edu",
+    license          = "MIT",
+    packages         = [
+      "exh", 
+      "exh.model", 
+      "exh.utils", 
+      "exh.prop", 
+      "exh.exts.gq", 
+      "exh.fol"
+    ],
+    install_requires = [
+        "numpy",
+        "IPython"
+    ],
+    zip_safe         = True
+)
